b56ed2134d69c7b5245a8a92125f5e8202f1fedd
Patrick Williams <pwmckenna@gmail.com>
2013-10-21 16:31:55 -0700

    making the jquery qrcode output printable

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -12,42 +12,11 @@
 			width		: 256,
 			height		: 256,
 			typeNumber	: -1,
-			correctLevel	: QRErrorCorrectLevel.H,
-                        background      : "#ffffff",
-                        foreground      : "#000000"
+			correctLevel	: QRErrorCorrectLevel.H
 		}, options);
 
-		var createCanvas	= function(){
-			// create the qrcode itself
-			var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
-			qrcode.addData(options.text);
-			qrcode.make();
-
-			// create canvas element
-			var canvas	= document.createElement('canvas');
-			canvas.width	= options.width;
-			canvas.height	= options.height;
-			var ctx		= canvas.getContext('2d');
-
-			// compute tileW/tileH based on options.width/options.height
-			var tileW	= options.width  / qrcode.getModuleCount();
-			var tileH	= options.height / qrcode.getModuleCount();
-
-			// draw in the canvas
-			for( var row = 0; row < qrcode.getModuleCount(); row++ ){
-				for( var col = 0; col < qrcode.getModuleCount(); col++ ){
-					ctx.fillStyle = qrcode.isDark(row, col) ? options.foreground : options.background;
-					var w = (Math.ceil((col+1)*tileW) - Math.floor(col*tileW));
-					var h = (Math.ceil((row+1)*tileW) - Math.floor(row*tileW));
-					ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);  
-				}	
-			}
-			// return just built canvas
-			return canvas;
-		}
-
 		// from Jon-Carlos Rivera (https://github.com/imbcmdth)
-		var createTable	= function(){
+		var createQRCode	= function(){
 			// create the qrcode itself
 			var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
 			qrcode.addData(options.text);
@@ -55,9 +24,8 @@
 			
 			// create table element
 			var $table	= $('<table></table>')
-				.css("width", options.width+"px")
-				.css("height", options.height+"px")
 				.css("border", "0px")
+				.css('white-space', 'none')
 				.css("border-collapse", "collapse")
 				.css('background-color', options.background);
 		  
@@ -70,10 +38,20 @@
 				var $row = $('<tr></tr>').css('height', tileH+"px").appendTo($table);
 				
 				for(var col = 0; col < qrcode.getModuleCount(); col++ ){
-					$('<td></td>')
-						.css('width', tileW+"px")
-						.css('background-color', qrcode.isDark(row, col) ? options.foreground : options.background)
-						.appendTo($row);
+					var td = $('<td></td>');
+					var width = tileW+"px";
+					var height = tileH+"px";
+					td.css('padding', '0px');
+					var image = $('<img/>');
+					image.css('width', width);
+					image.css('height', height);
+					if (qrcode.isDark(row, col)) {
+						image.attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+					} else {
+						image.attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+					}
+					image.appendTo(td);
+					td.appendTo($row);
 				}	
 			}
 			// return just built canvas
@@ -82,7 +60,7 @@
   
 
 		return this.each(function(){
-			var element	= options.render == "canvas" ? createCanvas() : createTable();
+			var element	= createQRCode();
 			$(element).appendTo(this);
 		});
 	};
